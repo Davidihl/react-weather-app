391df77e10006c803aa46a74f74a3843f1cc5e68
David Ihl <daih1985@gmail.com>
2023-05-08 23:33:36 +0200

    Fetch locationData, add save button -> add locationData to array

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import { useState } from 'react';
 import apiKey from './apiKey';
 
 export default function App() {
+  const [locationInput, setLocationInput] = useState('');
   const [location, setLocation] = useState('');
-  const [weather, setWeather] = useState('');
+  const [recentLocation, setRecentLocation] = useState([]);
   const key = apiKey();
+  const [savedItems, setSavedItems] = useState(false);
 
   // Click handle, fetching the location with input value
   const handleClick = async () => {
     try {
-      const data = await (
+      const locationData = await (
         await fetch(
-          `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${key}`,
+          `http://api.openweathermap.org/data/2.5/weather?q=${locationInput}&APPID=${key}`,
         )
       ).json();
-      setWeather(data);
+      setLocation(locationData);
     } catch (err) {
       console.log(err.message);
     }
+  };
+
+  const saveLocation = () => {
+    setSavedItems(true);
+    setRecentLocation([location, ...recentLocation]);
+    console.log(recentLocation);
   };
 
   return (
@@ -27,13 +35,63 @@ export default function App() {
         <label>
           Location
           <input
-            value={location}
-            onChange={(event) => setLocation(event.currentTarget.value)}
+            value={locationInput}
+            onChange={(event) => setLocationInput(event.currentTarget.value)}
           />
         </label>
         <button onClick={() => handleClick()}>Search</button>
       </div>
-      <div>{weather ? weather.name : ''}</div>
+      <div>
+        <section id="current">
+          {location ? (
+            <>
+              <h1>{location.name}</h1>
+              <div>
+                <p>Latitude: {location.coord.lat}</p>
+                <p>Longitude: {location.coord.lon}</p>
+              </div>
+              <div id="temperature">
+                <p>Current temperature: {location.main.temp}</p>
+                <p>Feels like: {location.main.feels_like}</p>
+              </div>
+              <div id="icon">
+                <div>
+                  <img
+                    alt={location.weather[0].description}
+                    src={`https://openweathermap.org/img/wn/${location.weather[0].icon}@2x.png`}
+                  />
+                </div>
+                <div>{location.weather[0].description}</div>
+              </div>
+              <div id="wind">
+                <div>Wind speed: {location.wind.speed}</div>
+              </div>
+              <button onClick={() => saveLocation()}>Save location</button>
+            </>
+          ) : (
+            ''
+          )}
+        </section>
+        {savedItems ? (
+          <section id="recent">
+            <h1>See also</h1>
+            {recentLocation.map((element) => (
+              <div key={`recent-${element.name}`}>
+                <div>{element.name}</div>
+                <div>{element.main.temp}</div>
+                <div>
+                  <img
+                    alt={element.weather[0].description}
+                    src={`https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`}
+                  />
+                </div>
+              </div>
+            ))}
+          </section>
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   );
 }
